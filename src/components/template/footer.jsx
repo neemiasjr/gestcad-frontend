5366109c9f1a45534309e0a40d983ef87763850f
Neemias Chagas de Souza Júnior <neemias.junior@tjdft.jus.br>
2021-07-23 02:55:06 -0300

    Primeiro commit..

---
+++ src/components/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './footer.css'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Projeto desenvolvido Neemias JR - desafio #2 - ecommerce.
+        </span>
+    </footer>
