b6bbbf73132adb265cd31b30828b61e0750b969c
Neemias Chagas de Souza Júnior <neemias.junior@tjdft.jus.br>
2021-07-26 00:12:58 -0300

    Alterando o componente nav.jsx

--- src/components/template/nav.jsx
+++ src/components/template/nav.jsx
@@ -7,7 +7,7 @@ export default props =>
     <aside className="menu-area">
         <nav className="menu">
             <Link to="/"> 
-                <i className="fa fa-home"></i> Home Teste
+                <i className="fa fa-home"></i> Home
             </Link>
             <Link to="/users">
                 <i className="fa fa-users"></i> Clientes
