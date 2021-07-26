48992429fae3379b2e20f9e16bfd6e1a63a69ae7
Neemias Chagas de Souza Júnior <neemias.junior@tjdft.jus.br>
2021-07-25 23:47:20 -0300

    Teste no github.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ecommerce - Loja de Instrumentos Musicais - MadeInBrazil
+## Projeto desenvolvido para o evento Gama Academy - Hiring Coders - 2021.
--- src/components/template/nav.jsx
+++ src/components/template/nav.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom'
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            <Link to="/">
-                <i className="fa fa-home"></i> Home
+            <Link to="/"> 
+                <i className="fa fa-home"></i> Home Teste
             </Link>
             <Link to="/users">
                 <i className="fa fa-users"></i> Clientes
